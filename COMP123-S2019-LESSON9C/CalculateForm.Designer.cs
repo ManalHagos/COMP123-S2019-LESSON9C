532615674891a3ef2a10100d52bc7812f485ad0f
Manal Yassin Hagos <ManalHagos@hotmail.com>
2019-07-08 15:54:01 -0400

    created base empty form

--- COMP123-S2019-LESSON9C/CalculateForm.Designer.cs
+++ COMP123-S2019-LESSON9C/CalculateForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace COMP123_S2019_LESSON9C
 {
-    partial class Form1
+    partial class CalculateForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CalculateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Name = "CalculateForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
